5f78534cbc50444eac77c6068b0ba801ea1c9708
vash <vashmar7@gmail.com>
2015-02-15 14:54:21 -0600

    login listener added

--- index.js
+++ index.js
@@ -89,7 +89,12 @@ app.post("/login", function(req, res){
 
 
 io.on('connection', function (socket) {
-  console.log("Connected to: " + JSON.stringify(socket));
+  console.log("Connected to: " + socket.id);
 
   socket.emit('hello', { hello: 'world' });
+
+  socket.on('Login', function(data){
+  	console.log("Login data: " + data);
+  	console.log(JSON.stringify(data));
+  })
 });

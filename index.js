93813337b030fad0053fec3d9952b679ef65c764
vash <vashmar7@gmail.com>
2015-02-16 15:13:32 -0600

    repeating hello bug fixed

--- index.js
+++ index.js
@@ -88,8 +88,6 @@ app.post("/login", function(req, res){
 io.on('connection', function (socket) {
   console.log("Connected to: " + socket.id);
 
-  socket.emit('Hello', { hello: 'world' });
-
   socket.on('Login', function(data){
   	console.log("Login data: " + data);
   	console.log(JSON.stringify(data));
